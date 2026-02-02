be72f98d558bb202e08a677eb2f82db182760de4
sharkawy02 <122478643+sharkawy02@users.noreply.github.com>
2026-02-02 04:11:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ function initContentCostChart() {
             labels: ['MaxGen', 'Evolve', 'Pure Ganic', 'Dr. M. Mahmoud', 'Muscle Add', 'Keep Going', 'Elements'],
             datasets: [{
                 label: 'Content Marketing Cost (EGP)',
-                data: [17500, 30000, 17500, 6500, 50000, 12500, 3500],
+                data: [17500, 30000, 17500, 6500, 50000, 15000, 3500],
                 backgroundColor: [
                     '#ffd700',
                     '#a371f7',
@@ -270,7 +270,7 @@ function initBudgetChart() {
             datasets: [
                 {
                     label: 'Content Marketing',
-                    data: [17500, 30000, 10000, 6500, 50000, 12500, 3500],
+                    data: [17500, 30000, 17500, 6500, 50000, 15000, 3500],
                     backgroundColor: '#ffd700',
                     borderRadius: 6
                 },
